f69d96f3db59fdffab0de3a05e1170bc9fa2cbf3
PytricioPUCV <patricio.hernandez.v@mail.pucv.cl>
2026-09-07 11:51:03 -0300

    fix: app components

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,23 +1,25 @@
 import { TestBed } from '@angular/core/testing';
-import { App } from './app';
+import { AppComponent } from './app.component'; 
 
-describe('App', () => {
+describe('AppComponent', () => { 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [App]
+      imports: [AppComponent] 
     }).compileComponents();
   });
 
   it('debe crear la aplicación', () => {
-    const fixture = TestBed.createComponent(App);
+    const fixture = TestBed.createComponent(AppComponent); 
     expect(fixture.componentInstance).toBeTruthy();
   });
 
   it('debe mostrar el título del catálogo', () => {
-    const fixture = TestBed.createComponent(App);
+    const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain('Catálogo de Recursos');
   });
 });
+
+//Update
